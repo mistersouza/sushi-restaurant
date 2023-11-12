a56a853c57835070931248f471476a3869311ff9
Mister Souza <mistersouza@icloud.com>
2023-11-13 00:24:43 +0100

    Install AOS animation and animate hero, about and popular

---
+++ assets/scripts/main.js
@@ -0,0 +1,16 @@
+// import assets
+import star from '../images/star.svg'
+import sushi12 from '../images/sushi-12.png'
+import sushi11 from '../images/sushi-11.png'
+import sushi10 from '../images/sushi-10.png'
+
+// import AOS
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+
+// init AOS
+AOS.init({
+    duration: 1000,
+    offset: 100,
+});
