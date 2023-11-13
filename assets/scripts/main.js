46f2343c0362b49b78661525532366919ee2c029
Mister Souza <mistersouza@icloud.com>
2023-11-13 14:22:09 +0100

    Gerrid of unessecary imports

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,9 +1,3 @@
-// import assets
-import star from '../images/star.svg'
-import sushi12 from '../images/sushi-12.png'
-import sushi11 from '../images/sushi-11.png'
-import sushi10 from '../images/sushi-10.png'
-
 // import AOS
 import AOS from "aos";
 import "aos/dist/aos.css";
